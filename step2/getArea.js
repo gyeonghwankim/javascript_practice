51d3f4d4f3ceb8da3cfc34d01dae20c28bed1023
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2020-04-16 13:51:26 +0900

    :newspaper: [step2] getArea.js 초기화

---
+++ step2/getArea.js
@@ -0,0 +1,15 @@
+/*
+    1. 다각형의 넓이를 구하는 함수 getArea, 함수 getAreaAvg 구현
+    2. 함수 호출순서를 알려주는 함수 printExecutionSequence 구현
+*/
+
+const figureTypes = ['circle', 'parallelogram', 'trapezoid'];
+const ARG_ERROR_MSG = '오류 발생! 올바른 인자값 입력해주세요';
+
+function getArea(){
+
+}
+
+function getAreaAvg(){
+
+}
