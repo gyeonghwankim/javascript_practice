5be3a0d480697d2ecc8b9bf3d787d403d2652978
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2020-04-16 17:07:25 +0900

    :pencil: [step2] 함수 getArea 구현

--- step2/getArea.js
+++ step2/getArea.js
@@ -1,4 +1,6 @@
 /*
+    Step2 다각형의 넓이
+
     1. 다각형의 넓이를 구하는 함수 getArea, 함수 getAreaAvg 구현
     2. 함수 호출순서를 알려주는 함수 printExecutionSequence 구현
 */
@@ -6,10 +8,32 @@
 const figureTypes = ['circle', 'parallelogram', 'trapezoid'];
 const ARG_ERROR_MSG = '오류 발생! 올바른 인자값 입력해주세요';
 
-function getArea(){
+function getArea(figureType, ...values){
+    let ret;
 
+    if(figureType === 'circle') ret = getCircleArea(...values);
+    else if(figureType === 'parallelogram') ret = getParallelogramArea(...values);
+    else if(figureType == 'trapezoid') ret = getTrapezoid(...values);
+    
+    if(ret === undefined || isNaN(ret)) return ARG_ERROR_MSG;
+    return ret;
+}
+
+function getCircleArea(radius){
+    return Math.pow(radius,2) * Math.PI;
+}
+
+function getParallelogramArea(width, height){
+    return width * height;
+}
+
+function getTrapezoid(upperSide, lowerSide, height){
+    return upperSide * lowerSide * height;
 }
 
 function getAreaAvg(){
-
 }
+
+console.log(getArea('circle', 10, 3.14));
+console.log(getArea('parallelogram', 10, 15));
+console.log(getArea('trapezoid', 10));
