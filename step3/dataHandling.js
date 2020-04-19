4125d6adb367e4eb0a5b2b678bc529df7e7a95d8
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2020-04-19 22:46:42 +0900

    :pencil: [step3] 함수 customReduce 구현

--- step3/dataHandling.js
+++ step3/dataHandling.js
@@ -16,8 +16,23 @@ function getMatchedType(json, type){
     });
 
     getName(json, type);
-    
+
     return `${type} 타입의 데이터는 총 ${matchedName.length}개이며, ${matchedName.join(", ")} 입니다`
+}
+
+Array.prototype.customReduce = function(callback, initialValue){
+    const index = 0;
+
+    if(initialValue === undefined){
+        initialValue = this[0];
+        index = 1;
+    }
+
+    const innerFunc = (accumulator, currentIndex) => {
+        if(currentIndex == this.length) return accumulator;
+        return innerFunc(callback(accumulator, this[currentIndex]), currentIndex+1);
+    }
+    return innerFunc(initialValue, index);
 }
 
 console.log(getMatchedType(exampleData, "samsung"));
