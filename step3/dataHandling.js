39336a2fea05eb67497e84b65f063ab0475925e8
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2020-04-19 18:54:33 +0900

    :newspaper: [step3] dataHandling.js 초기화 및 예시 데이터 추가

---
+++ step3/dataHandling.js
@@ -0,0 +1,10 @@
+/*
+    Step3 데이터 다루기
+
+    1. 함수 customReduce, customFilter, customForeach, customMap 구현
+    2. type이 일치하는 obejct의 name을 출력하는 함수 getMatchedType 구현
+*/
+
+const jsonData = require('./exampleData');
+
+console.log(jsonData);
---
+++ step3/exampleData.js
@@ -0,0 +1,82 @@
+const jsonData= [{
+	"id": 1,
+	"name": "noah",
+	"phone": "010-0000-0000",
+	"type": "samsung",
+	"childnode": [{
+		"id": 11,
+		"name": "mason",
+		"phone": "010-0000-1111",
+		"type": "lg",
+		"childnode": [{
+				"id": 115,
+				"name": "logan",
+				"phone": "211-1111-0000",
+				"type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "lucas",
+					"phone": "010-444-0002",
+					"type": "sk",
+					"childnode": [{
+							"id": 11592,
+							"name": "adela",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							"name": "benjamin",
+							"phone": "010-000-3333",
+							"type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "charlotte",
+				"phone": "444-111-0200",
+				"type": "samsung",
+				"childnode": [{
+					"id": 1168,
+					"name": "joseph",
+					"phone": "010-222-0000",
+					"type": "samsung",
+					"childnode": [{
+						"id": 11689,
+						"name": "david",
+						"phone": "010-000-0000",
+						"type": "samsung",
+						"childnode": [{
+								"id": 116890,
+								"name": "grace",
+								"phone": "010-000-0000",
+								"type": "sk",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								"name": "emma",
+								"phone": "010-0000-0000",
+								"type": "lg",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "isabel",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}];
+
+module.exports = jsonData;
