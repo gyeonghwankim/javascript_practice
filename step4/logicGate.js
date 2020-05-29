d59858dd04dcdd4219f701dbd8a8da03f877576b
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2020-05-29 15:36:06 +0900

    [ADD] step4 : logicGate.js 초기화 및 함수 nand, nor, xor 구현

---
+++ step4/logicGate.js
@@ -0,0 +1,16 @@
+/*
+    Step4 논리 게이트
+
+    1. BOOL 타입의 매개변수를 두 개 가지고 결과값으로 BOOL 타입을 리턴하는 NAND, NOR, XOR를 구현
+    2. half-adder, full-adder 구현
+    3. 10진수에서 2진수로, 2진수에서 10진수로 변환하는 함수 구현
+*/
+
+const exampleInput = [ [true, true], [true, false], [false, true], [false, false]];
+
+const nand = (paramA, paramB) => !(paramA && paramB);
+const nor = (paramA, paramB) => !(paramA || paramB);
+const xor = (paramA, paramB) => (!paramA && paramB) || (paramA && !paramB);
+
+
+// exampleInput.forEach((currentValue) => console.log(`NAND(${currentValue}) => ${nand(...currentValue)}`));
